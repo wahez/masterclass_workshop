5cf993c84fc2f5b1ce0e6908f58afff5a807b695
Walther Zwart <walther.zwart@gmail.com>
2018-11-27 22:56:54 +0100

    Removed strings

--- card.h
+++ card.h
@@ -5,34 +5,47 @@
 
 namespace workshop {
 
+enum class Color
+{
+	spades,
+	hearts,
+	clubs,
+	diamonds
+};
+
+enum class Type
+{
+	ace = 1,
+	two = 2,
+	three = 3,
+	four = 4,
+	five = 5,
+	six = 6,
+	seven = 7,
+	eight = 8,
+	nine = 9,
+	ten = 10,
+	jack,
+	queen,
+	king
+};
+
 class CardBase
 {
 public:
 	virtual ~CardBase();
-	virtual const std::string& color() const = 0;
-	virtual const std::string& type() const = 0;
+
+	virtual Color color() const = 0;
+	virtual Type type() const = 0;
 };
-
-inline std::ostream& operator<<(std::ostream& os, const CardBase& card)
-{
-	os << '[' << card.type() << " of " << card.color() << ']';
-	return os;
-}
-
-inline bool operator<(const CardBase& lhs, const CardBase& rhs)
-{
-	if (lhs.color() != rhs.color())
-		return lhs.color() < rhs.color();
-	return lhs.type() < rhs.type();
-}
 
 class Card : public CardBase
 {
 public:
-	Card(std::string c, std::string t) : type_(std::move(t)), color_(std::move(c)) {}
+	Card(Color color, Type type) : type_(type), color_(color) {}
 
-	const std::string& color() const override { return color_; }
-	const std::string& type() const override { return type_; }
+	Color color() const override { return color_; }
+	Type type() const override { return type_; }
 
 	friend bool operator<(const Card& lhs, const Card& rhs)
 	{
@@ -40,8 +53,56 @@ public:
 	}
 
 private:
-	std::string type_;
-	std::string color_;
+	Type type_;
+	Color color_;
 };
+
+inline std::ostream& operator<<(std::ostream& os, const Card& card)
+{
+	os << '[';
+	switch (card.type())
+	{
+	case Type::ace:
+		os << "ace";
+		break;
+	case Type::two:
+	case Type::three:
+	case Type::four:
+	case Type::five:
+	case Type::six:
+	case Type::seven:
+	case Type::eight:
+	case Type::nine:
+	case Type::ten:
+		os << static_cast<int>(card.type());
+		break;
+	case Type::jack:
+		os << "jack";
+		break;
+	case Type::queen:
+		os << "queen";
+		break;
+	case Type::king:
+		os << "king";
+		break;
+	}
+	os << " of ";
+	switch (card.color())
+	{
+	case Color::spades:
+		os << "spades";
+		break;
+	case Color::hearts:
+		os << "hearts";
+		break;
+	case Color::clubs:
+		os << "clubs";
+		break;
+	case Color::diamonds:
+		os << "diamonds";
+		break;
+	}
+	return os;
+}
 
 }
--- card_generator.cc
+++ card_generator.cc
@@ -4,50 +4,13 @@
 
 namespace workshop {
 
-std::string type_to_string(int type)
-{
-	switch (type)
-	{
-	case 1:
-		return "ace";
-	case 2:
-	case 3:
-	case 4:
-	case 5:
-	case 6:
-	case 7:
-	case 8:
-	case 9:
-	case 10:
-		return std::to_string(type);
-	case 11:
-		return "jack";
-	case 12:
-		return "queen";
-	case 13:
-		return "king";
-	}
-	throw std::runtime_error("Unkown card type");
-}
-
 Card CardGenerator::get_random_card()
 {
-    static std::uniform_int_distribution<int> color_dist{1, 4};
-    static std::uniform_int_distribution<int> type_dist{2, 13};
-    const auto type = type_to_string(type_dist(engine));
-    int color = color_dist(engine);
-    switch (color)
-    {
-    case 1:
-        return Card{"hearts", type};
-    case 2:
-        return Card{"clubs", type};
-    case 3:
-        return Card{"diamonds", type};
-    case 4:
-        return Card{"spades", type};
-    }
-    throw std::runtime_error("Unknown card color");
+    static std::uniform_int_distribution<int> color_dist{static_cast<int>(Color::spades), static_cast<int>(Color::diamonds)};
+    static std::uniform_int_distribution<int> type_dist{static_cast<int>(Type::ace), static_cast<int>(Type::king)};
+    const auto color = static_cast<Color>(color_dist(engine));
+    const auto type = static_cast<Type>(type_dist(engine));
+    return Card{color, type};
 }
 
 }
